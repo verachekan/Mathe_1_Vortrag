7a78135044ee57a9b84e4dfbf9fa4f506c926f1e
Vera <veracheckan@mail.ru>
2018-06-20 22:13:41 +0200

    Many fixes

--- minmax2.cpp
+++ minmax2.cpp
@@ -2,7 +2,7 @@ int minmax(Zustands-Knoten k):
   if(k ist Blatt){
     return k.getBewertung
   } else {
-  for(alle Kindknoten kind von k){
-    res = - min(res, Bewertung(k))
+  	for(alle Kindknoten kind von k){
+    	res = - min(res, Bewertung(k))
     }
   }
